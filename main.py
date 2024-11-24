30c50c7dc68171e537168c73272019672787117c
Huno <136697789+hunxio@users.noreply.github.com>
2024-11-24 03:05:10 +0100

    Added loop to keep the program always going, changed message visualization, added (not implemented yet) new commands, added small delay

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 from contact import Contact
 from commands import Commands
+import time
 
 
 def main():
-    user_option = input(
-        "Welcome User\nWhat do you wish to do?\n1)Add contact  2)Access Contact List\n3)Search Contact\n"
-    )
-    if user_option == "1":
-        info = Commands().form()
-        name, last_name, number, email = (
-            info[0],
-            info[1],
-            info[2],
-            info[3],
+    while True:
+        user_option = input(
+            "\nWhat do you wish to do?\n1)Add contact\n2)Access Contact List\n3)Search Contact\n4)Edit Contact\n5)Delete Contact\n6)Exit\nOption: "
         )
-        new_contact = Contact(name, last_name, number, email)
-        Commands().add(new_contact)
-    elif user_option == "2":
-        Commands().display()
-    elif user_option == "3":
-        print(Commands().search())
-    else:
-        print("Option not available, exiting application.")
+        if user_option == "1":
+            #add()
+            info = Commands().form()
+            name, last_name, number, email = (
+                info[0],
+                info[1],
+                info[2],
+                info[3],
+            )
+            new_contact = Contact(name, last_name, number, email)
+            Commands().add(new_contact)
+        elif user_option == "2":
+            #display()
+            Commands().display()
+        elif user_option == "3":
+            #search()
+            print(Commands().search())
+        elif user_option == "4":
+            # edit()
+            ...
+        elif user_option == "5":
+            #remove()
+            ...
+        elif user_option == "6":
+            #exit program
+            ...
+        else:
+            print("Option not available.")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
