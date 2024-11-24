9daeb6d8b078e64d7587482c37a84d38d8c7c625
Huno <136697789+hunxio@users.noreply.github.com>
2024-11-24 01:53:29 +0100

    added command to search for a contact using the first name

--- commands.py
+++ commands.py
@@ -10,7 +10,9 @@ class Commands:
         self.path = path
 
     def add(self, contact) -> None:
-        infos = [contact.name, contact.last_name, contact.number, contact.email]
+        infos = [
+            contact.name, contact.last_name, contact.number, contact.email
+        ]
         if not os.path.isfile(self.path):
             headers = ["name", "last name", "number", "email"]
             with open("contacts.csv", "w", newline="") as file:
@@ -25,8 +27,10 @@ class Commands:
         with open("contacts.csv", "r", newline="") as file:
             file_reader = csv.DictReader(file)
             for row in file_reader:
-                name, last_name, number, email = row["name"], row["last name"], row["number"], row["email"]
-                print(f"\n{name} {last_name}\nNumber: {number}\nEmail: {email}")
+                name, last_name, number, email = row["name"], row[
+                    "last name"], row["number"], row["email"]
+                print(
+                    f"\n{name} {last_name}\nNumber: {number}\nEmail: {email}")
 
     @staticmethod
     def form() -> list[str]:
@@ -35,3 +39,14 @@ class Commands:
         number_ipt = input("Number:\n")
         email_ipt = input("Email:\n").lower()
         return [name_ipt, last_name_ipt, number_ipt, email_ipt]
+
+    @staticmethod
+    def search() -> str:
+        contact_name_ipt = input(
+            "\nEnter the name of the contact you are looking for:\n").lower()
+        with open("contacts.csv", "r") as file:
+            file_reader = csv.DictReader(file)
+            for row in file_reader:
+                if row["name"].lower() == contact_name_ipt:
+                    return f"\nContact details for {contact_name_ipt.capitalize()}:\nLast Name: {row['last name']}\nNumber: {row['number']}\nEmail: {row['email']}"
+        return "\nContact not found."
