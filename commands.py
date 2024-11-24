a11327c306cf07c0693eaf8d7c5f624e0847aca8
Huno <136697789+hunxio@users.noreply.github.com>
2024-11-24 03:01:39 +0100

    added delay after outputs

--- commands.py
+++ commands.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import time
 
 # Folder path, contacts.csv is added to path to verify if it exist or not for the add() method.
 PATH = os.path.join(os.path.dirname(__file__), 'contacts.csv')
@@ -30,21 +31,14 @@ class Commands:
             with open("contacts.csv", "r", newline="") as file:
                 file_reader = csv.DictReader(file)
                 for row in file_reader:
-                        name, last_name, number, email = row["name"], row[
-                            "last name"], row["number"], row["email"]
-                        print(
-                            f"\n{name} {last_name}\nNumber: {number}\nEmail: {email}")
+                    name, last_name, number, email = row["name"], row[
+                        "last name"], row["number"], row["email"]
+                    print(
+                        f"\n{name} {last_name}\nNumber: {number}\nEmail: {email}"
+                    )
+                    time.sleep(0.4)
         except Exception:  #Exits the command with an informational message (Error: FileNotFoundError, ).
             print("Please add a contact to create a contact list.")
-
-    # < Not a command to manage the contact list >
-    @staticmethod
-    def form() -> list[str]:
-        name_ipt = input("Name:\n").capitalize()
-        last_name_ipt = input("Last Name:\n").capitalize()
-        number_ipt = input("Number:\n")
-        email_ipt = input("Email:\n").lower()
-        return [name_ipt, last_name_ipt, number_ipt, email_ipt]
 
     @staticmethod
     def search() -> str:
@@ -54,5 +48,22 @@ class Commands:
             file_reader = csv.DictReader(file)
             for row in file_reader:
                 if row["name"].lower() == contact_name_ipt:
+                    time.sleep(0.4)
                     return f"\nContact details for {contact_name_ipt.capitalize()}:\nLast Name: {row['last name']}\nNumber: {row['number']}\nEmail: {row['email']}"
+        time.sleep(0.4)
         return "\nContact not found."
+
+    def edit():
+        ...
+
+    def remove():
+        ...
+
+    # < Not a command to manage the contact list >
+    @staticmethod
+    def form() -> list[str]:
+        name_ipt = input("Name:\n").capitalize()
+        last_name_ipt = input("Last Name:\n").capitalize()
+        number_ipt = input("Number:\n")
+        email_ipt = input("Email:\n").lower()
+        return [name_ipt, last_name_ipt, number_ipt, email_ipt]
