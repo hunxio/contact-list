5a6c479d4557121729cf7d95ed01297617b3f349
Huno <136697789+hunxio@users.noreply.github.com>
2024-11-26 22:19:26 +0100

    fixed issue with edit method, now it properly edits the contact informations when used.

--- commands.py
+++ commands.py
@@ -4,18 +4,16 @@ import time
 import sys
 
 # Folder path, contacts.csv is added to path to verify if it exist or not for the add() method.
-PATH = os.path.join(os.path.dirname(__file__), 'contacts.csv')
+PATH = os.path.join(os.path.dirname(__file__), "contacts.csv")
 
 
 class Commands:
 
     def __init__(self, path: str = PATH):
         self.path = path
-    #TODO: If the file exists but there is no header, it needs to be added.
+
     def add(self, contact) -> None:
-        infos = [
-            contact.name, contact.last_name, contact.number, contact.email
-        ]
+        infos = [contact.name, contact.last_name, contact.number, contact.email]
         if not os.path.isfile(self.path):
             headers = ["name", "last name", "number", "email"]
             with open("contacts.csv", "w", newline="") as file:
@@ -33,19 +31,25 @@ class Commands:
             with open("contacts.csv", "r", newline="") as file:
                 file_reader = csv.DictReader(file)
                 for row in file_reader:
-                    name, last_name, number, email = row["name"], row[
-                        "last name"], row["number"], row["email"]
+                    name, last_name, number, email = (
+                        row["name"],
+                        row["last name"],
+                        row["number"],
+                        row["email"],
+                    )
                     print(
                         f"\n{name} {last_name}\nNumber: {number}\nEmail: {email}"
                     )
                     time.sleep(0.4)
-        except Exception:  #Exits the command with an informational message (Error: FileNotFoundError).
+        # Exits the command with an informational message (Error: FileNotFoundError).
+        except Exception:
             print("Please add a contact to create a contact list.")
 
     @staticmethod
     def search() -> str:
         contact_name_ipt = input(
-            "\nEnter the name of the contact you are looking for:\n").lower()
+            "\nEnter the name of the contact you are looking for:\n"
+        ).lower()
         with open("contacts.csv", "r") as file:
             file_reader = csv.DictReader(file)
             for row in file_reader:
@@ -55,34 +59,44 @@ class Commands:
         time.sleep(0.4)
         return "\nContact not found."
 
-    #TODO: The list does not update, to be fixed tomorrow (in progress)
     def edit() -> str:
         contact_name = input("\nInsert contact's name: ").lower()
+        data = []  # it will store the old data
         with open("contacts.csv", "r") as file:
             file_reader = csv.DictReader(file)
-            udpated_data = []
-            fieldnames = file_reader.fieldnames # store the fieldnames for the header later
+            fieldnames = (
+                file_reader.fieldnames
+            )  # store the fieldnames for the header later
             for row in file_reader:
+                # loops for each row in the csv file and adds it to data
+                data.append(row)
+            for row in data:
+                # it searches for the name, the value name will then store the name of the contact we want to change the infos
                 if row["name"].lower() == contact_name:
-                    user_ipt = input("\nWhat do you want to edit?\n1) Number\n2) Email\n")
+                    user_ipt = input(
+                        "\nWhat do you want to edit?\n1) Number\n2) Email\n"
+                    )
                     if user_ipt == "1":
                         new_number = input("What will the new number be?\n")
-                        row["number"] == new_number
+                        row["number"] = new_number
+                        break
                     elif user_ipt == "2":
                         new_email = input("What will the new email be?\n")
-                        row["email"] == new_email
+                        row["email"] = new_email
+                        break
                     else:
                         return "Option not available.\n"
-        with open("contacts.csv", "w") as new_modified_file:
+
+        with open("contacts.csv", "w", newline="") as new_modified_file:
             file_writer = csv.DictWriter(new_modified_file, fieldnames=fieldnames)
-            file_writer.writeheader() # add the header again
-            file_writer.writerows(rows) #add the now update rows 
-        time.sleep(0.4)
+            file_writer.writeheader()  # adds the header
+            file_writer.writerows(data)  # adds the rows
+            return f"Updated informations for {contact_name.capitalize()}"
+
+        time.sleep(0.2)
         return f"\nNo contact found with the name {contact_name.capitalize()}."
 
-
-    def remove():
-        ...
+    def remove(): ...
 
     @staticmethod
     def exit():
